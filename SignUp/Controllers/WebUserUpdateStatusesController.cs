b11140b6f3c694dd11bb4ce31bd5259d7df14dff
Ernst <ernst@hotmail.co.nz>
2022-02-05 14:30:28 +1300

    User List Take one

---
+++ SignUp/Controllers/WebUserUpdateStatusesController.cs
@@ -0,0 +1,49 @@
+﻿using SignUp.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace SignUp.Controllers
+{
+    public class WebUserUpdateStatusesController : ApiController
+    {
+
+    // PUT: api/WebUsers/5
+    [ResponseType(typeof(void))]
+    public IHttpActionResult PutWebUser(string id, dynamic webUser)
+    {
+      //if (!ModelState.IsValid)
+      //{
+      //  return BadRequest(ModelState);
+      //}
+
+      //if (id != webUser["UserId"])
+      //{
+      //  return BadRequest();
+      //}
+      var status = webUser["Status"];
+
+      using (var db = new Entities())
+      {
+        var User = db.WebUsers.Single(u => u.UserId == id);
+        User.UserNotActive = status;
+        try
+        {
+          db.SaveChanges();
+        }
+        catch (DbUpdateException ex)
+        {
+          return BadRequest(ex.Message);
+        }
+      }
+
+      return StatusCode(HttpStatusCode.NoContent);
+    }
+
+  }
+}
---
+++ SignUp/Controllers/WebUsersByCompaniesController.cs
@@ -0,0 +1,80 @@
+﻿using SignUp.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SignUp.Controllers
+{  
+    public class WebUsersByCompaniesController : ApiController
+    {
+    [HttpGet]
+    public IHttpActionResult GetUsers(int id)
+    {
+ 
+      DataTable dataTable = new DataTable();
+      string connString = ConfigurationManager.ConnectionStrings["IdentityDemoConnection"].ConnectionString;
+      string query = @"Select webuser.UserId,
+        	UserFirstName + ' ' + coalesce(UserLastName, '') as Username,
+    	    UserEmail,
+        	UserPhoneNumber,
+        	DepartmentName,
+          FunctionName,
+        	UserNotActive,
+        	Profiles.ProfileName,
+        	Company.CompanyName,
+        	count(linkprojects.ProjectCode) as Projects,
+        	count(Training.TrainingId) as Training
+        from WebUser	
+          inner join Company on WebUser.UserCompanyCode = Company.CompanyCode
+          inner join profiles on WebUser.UserProfileCode = Profiles.ProfileLevel
+          Left Join Departments on Departments.DepartmentId = WebUser.UserDepartmentCode
+          Left Join Functions on WebUser.UserFunctionCode = Functions.FunctionId
+          left join LinkProjects on WebUser.userId = LinkProjects.UserCode
+          left join Projects on LinkProjects.ProjectCode = Projects.ProjectCode
+          Left Join Training on Training.UserId = WebUser.UserId
+        Where Company.CompanyId = '" + id + "'  " +
+        "Group by webUser.UserId, " +
+          "UserFirstName, " +
+          "UserlastName, " +
+          "UserEmail, " +
+          "UserPhoneNumber, " +
+          "UserDepartmentCode, " +
+          "UserFunctionCode, " +
+           "UserProfileCode, " +
+          "UserNotActive, " +
+          "CompanyName, " +
+          "ProfileName, " +
+          "DepartmentName, " +
+          "FunctionName";
+  
+      SqlConnection conn = new SqlConnection(connString);
+      SqlCommand cmd = new SqlCommand(query, conn);
+      SqlDataAdapter da = new SqlDataAdapter(cmd);
+      try
+      {
+        conn.Open();
+        da.Fill(dataTable);
+      }
+      catch (Exception ex)
+      {
+        return BadRequest(ex.Message);
+      }
+
+      conn.Close();
+      da.Dispose();
+
+      if (dataTable == null)
+      {
+        return NotFound();
+      }
+
+      return Ok(dataTable);
+    }
+  }
+}
