6f974ea471a13db0f86442a502cc42440d138b80
Ernst <ernst@hotmail.co.nz>
2022-03-07 13:18:40 +1300

    2022/03/07-02 Fix Training View

--- SignUp/Controllers/Training/TrainingByCompanyIdController.cs
+++ SignUp/Controllers/Training/TrainingByCompanyIdController.cs
@@ -44,35 +44,66 @@ namespace SignUp.Controllers
     {
       DataTable dataTable = new DataTable();
       string connString = ConfigurationManager.ConnectionStrings["IdentityDemoConnection"].ConnectionString;
-      string query = @"SELECT WebUser.UserFirstName
-                              ,WebUser.UserLastName
-                              ,train.TrainingId
-                              ,train.UserId
-                              ,coalesce(train.CourseName, '') CourseName
-                              ,coalesce(train.CourseDate, '') CourseDate
-                              ,coalesce(train.ExpiryDate, '') ExpiryDate
-                              ,coalesce(train.CompetencyLevel, '') CompetencyLevel
-                              ,coalesce(train.ProviderName, '') ProviderName
-                              ,train.LastUpdateBy
-                              ,coalesce(train.UpdateDate, '') UpdateDate
-  	                          ,count(FD.Id) as Docs
-                          FROM WebUser 
-                            Inner Join Company on Company.CompanyCode = WebUser.UsercompanyCode
-                            Inner Join Training train on train.UserId = WebUser.UserId
-                            left Join FileData as FD on FD.ParentId = train.TrainingId 
-                                 AND FD.ParentName = 'Training'   
-                          Where Company.CompanyId = " + id + " " +
-                         "Group by WebUser.UserFirstName " +
-                              ", WebUser.UserLastName " +
-                              ",train.TrainingId " +
-                              ",train.UserId " +
-                              ",train.CourseName" +
-                              ",train.CourseDate" +
-                              ",train.ExpiryDate" +
-                              ",train.CompetencyLevel" +
-                              ",train.ProviderName" +
-                              ",train.LastUpdateBy " +
-                              ",train.UpdateDate";
+      string query =
+        @"SELECT WebUser.UserFirstName
+          ,WebUser.UserLastName
+          ,train.TrainingId
+          ,train.UserId
+          ,coalesce(train.CourseName, '') CourseName
+          ,coalesce(train.CourseDate, '') CourseDate
+          ,coalesce(train.ExpiryDate, '') ExpiryDate
+          ,coalesce(train.CompetencyLevel, '') CompetencyLevel
+          ,coalesce(train.ProviderName, '') ProviderName
+          ,train.LastUpdateBy
+          ,coalesce(train.UpdateDate, '') UpdateDate
+  	      ,count(FD.Id) as Docs
+        FROM WebUser 
+          Inner Join Company on Company.CompanyCode = WebUser.UsercompanyCode
+          Inner Join Training train on train.UserId = WebUser.UserId
+          left Join FileData as FD on FD.ParentId = train.TrainingId 
+            AND FD.ParentName = 'Training'   
+        Where Company.CompanyId =  '" + id + "' " +
+        "Group by WebUser.UserFirstName " +
+        "  , WebUser.UserLastName " +
+        "  ,train.TrainingId " +
+        "  ,train.UserId " +
+        "  ,train.CourseName " +
+        "  ,train.CourseDate " +
+        "  ,train.ExpiryDate " +
+        "  ,train.CompetencyLevel " +
+        "  ,train.ProviderName " +
+        "  ,train.LastUpdateBy " +
+        "  ,train.UpdateDate " +
+        "Union " +
+        "SELECT CompanyStaff.StaffName " +
+        "  , CompanyStaff.StaffSurname " +
+        "  ,train.TrainingId " +
+        "  ,cast(train.UserId as varchar(6)) " +
+        "  ,coalesce(train.CourseName, '') CourseName " +
+        "  ,coalesce(train.CourseDate, '') CourseDate " +
+        "  ,coalesce(train.ExpiryDate, '') ExpiryDate " +
+        "  ,coalesce(train.CompetencyLevel, '') CompetencyLevel " +
+        "  ,coalesce(train.ProviderName, '') ProviderName " +
+        "  ,train.LastUpdateBy " +
+        "  ,coalesce(train.UpdateDate, '') UpdateDate " +
+        "  ,count(FD.Id) as Docs " +
+        "FROM CompanyStaff " +
+        "  Inner Join Company on Company.CompanyCode = CompanyStaff.CompanyCode " +
+        "  Inner Join Training train on train.UserId = cast(CompanyStaff.StaffId as varchar(6)) " +
+        "  left Join FileData as FD on FD.ParentId = train.TrainingId " +
+        "    AND FD.ParentName = 'Training' " +
+        "Where Company.CompanyId =  '" + id + "' " +
+        "Group by CompanyStaff.StaffName " +
+        "  , CompanyStaff.StaffSurName " +
+        "  ,train.TrainingId " +
+        "  ,train.UserId " +
+        "  ,train.CourseName " +
+        "  ,train.CourseDate " +
+        "  ,train.ExpiryDate " +
+        "  ,train.CompetencyLevel " +
+        "  ,train.ProviderName " +
+        "  ,train.LastUpdateBy " +
+        "  ,train.UpdateDate";
 
       SqlConnection conn = new SqlConnection(connString);
       SqlCommand cmd = new SqlCommand(query, conn);
