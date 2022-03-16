bc7745212a9e4f41974b781b79fb3c9f20451090
Ernst <ernst@hotmail.co.nz>
2022-03-16 19:57:04 +1300

    Before Create Check List

---
+++ SignUp/Controllers/StepBack/StepBackByIdsController.cs
@@ -0,0 +1,32 @@
+﻿using SignUp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SignUp.Controllers.StepBack
+{
+    public class StepBackByIdsController : ApiController
+  {
+    private Entities db = new Entities();
+    [HttpGet]
+    public IHttpActionResult StepBackById(int id)
+    {
+      var list = from s in db.Stepbacks
+                 join user in db.WebUsers on s.AddedBy equals user.Username
+                 where s.LogId == id
+                 select new
+                 {
+                   s, 
+                   user.UserFirstName,
+                 };
+      if (list == null)
+      {
+        return NotFound();
+      }
+      return Ok(list);
+    }
+  }
+}
