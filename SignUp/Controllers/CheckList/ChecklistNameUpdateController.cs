792eacf288aa40af511aa9df26df965ec65973ea
Ernst <ernst@hotmail.co.nz>
2022-03-19 09:13:05 +1300

    2022/03/16-02 After Checklist Master

---
+++ SignUp/Controllers/CheckList/ChecklistNameUpdateController.cs
@@ -0,0 +1,34 @@
+﻿using SignUp.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SignUp.Controllers.CheckList
+{
+    public class ChecklistNameUpdateController : ApiController
+  {
+    private Entities db = new Entities();
+    [HttpPut]
+    public IHttpActionResult PutChecklistNameUpdate(string id, dynamic myParams)
+    {
+      var newName = myParams["newName"];
+      (from c in db.ChecklistMasters
+       where c.CheckListUID == id
+       select c).ToList()
+          .ForEach(x => x.CheckListName = newName);
+      try
+      {
+        db.SaveChanges();
+      }
+      catch (DbUpdateException ex)
+      {
+        return BadRequest(ex.Message);
+      }
+      return Ok();
+    }
+  }
+}
