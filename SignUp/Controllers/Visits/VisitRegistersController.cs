bcedfcbfbe55ec29813730e2f403d75266f3d53a
Ernst <ernst@hotmail.co.nz>
2022-03-30 07:26:06 +1300

    2022/03/26 Visit Regiser

---
+++ SignUp/Controllers/Visits/VisitRegistersController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using SignUp.Models;
+
+namespace SignUp.Controllers.Visits
+{
+  [AllowAnonymous]
+    public class VisitRegistersController : ApiController
+    {
+        private Entities db = new Entities();
+
+        // GET: api/VisitRegisters
+        public IQueryable<VisitRegister> GetVisitRegisters()
+        {
+            return db.VisitRegisters;
+        }
+
+        // GET: api/VisitRegisters/5
+        [ResponseType(typeof(VisitRegister))]
+        public IHttpActionResult GetVisitRegister(int id)
+        {
+            VisitRegister visitRegister = db.VisitRegisters.Find(id);
+            if (visitRegister == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(visitRegister);
+        }
+
+        // PUT: api/VisitRegisters/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutVisitRegister(int id, VisitRegister visitRegister)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != visitRegister.VisitId)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(visitRegister).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!VisitRegisterExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/VisitRegisters
+        [AllowAnonymous]
+        [ResponseType(typeof(VisitRegister))]
+        public IHttpActionResult PostVisitRegister(VisitRegister visitRegister)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.VisitRegisters.Add(visitRegister);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = visitRegister.VisitId }, visitRegister);
+        }
+
+        // DELETE: api/VisitRegisters/5
+        [ResponseType(typeof(VisitRegister))]
+        public IHttpActionResult DeleteVisitRegister(int id)
+        {
+            VisitRegister visitRegister = db.VisitRegisters.Find(id);
+            if (visitRegister == null)
+            {
+                return NotFound();
+            }
+
+            db.VisitRegisters.Remove(visitRegister);
+            db.SaveChanges();
+
+            return Ok(visitRegister);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool VisitRegisterExists(int id)
+        {
+            return db.VisitRegisters.Count(e => e.VisitId == id) > 0;
+        }
+    }
+}
---
+++ SignUp/Controllers/Visits/VisitsByCompanyCodesController.cs
@@ -0,0 +1,50 @@
+﻿using SignUp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SignUp.Controllers.Visits
+{
+
+  public class VisitsByCompanyCodesController : ApiController
+  {
+    private Entities db = new Entities();
+    [HttpGet]
+    public IHttpActionResult VisitsByCompanyCode(string id)
+    {
+      var visits = from m in db.VisitRegisters
+                     where m.CompanyCode == id
+                     select new
+                     {
+                       m.VisitId,
+                       m.CompanyCode,
+                       m.DateIn,
+                       DateOut = m.DateOut ?? "",
+                       m.Name,
+                       Signature = m.Signature ?? "",
+                       Company = m.Company ?? "",
+                       Purpose = m.Purpose ?? "",
+                       Comments = m.Comments ?? "",
+                       WhoVisit = m.WhoVisit ?? "",
+                       TimeIn = m.TimeIn ?? "",
+                       TimeOut = m.TimeOut ?? "",
+                       m.SignatureOut,
+                       DeviceId = m.DeviceId ?? "",
+                       Longitude = m.Longitude ?? "",
+                       Latitude = m.Latitude ?? "",
+                       m.DeviceType,
+                       m.IsStaff,
+                       m.IsLogin
+                     };
+      visits.OrderBy(x => x.DateIn);
+      if (visits == null)
+      {
+        return NotFound();
+      }
+      return Ok(visits);
+    }
+  }
+}
