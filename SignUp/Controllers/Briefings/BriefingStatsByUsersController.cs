fbc99770b676754feb4964a3b36f16a0a5469470
Ernst <ernst@hotmail.co.nz>
2022-10-16 00:08:12 +1300

    Basic Development Done - Before Testing

---
+++ SignUp/Controllers/Briefings/BriefingStatsByUsersController.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Web.Http;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace SignUp.Controllers.Briefings
+{
+    public class BriefingStatsByUsersController : ApiController
+  {
+    [HttpGet]
+    public IHttpActionResult BriefingStatsByCompanyCode(string id)
+    {
+      DataTable dataTable = new DataTable();
+      string connString = ConfigurationManager.ConnectionStrings["IdentityDemoConnection"].ConnectionString;
+      string query =
+        @"SET DATEFIRST 1
+          SELECT 'Briefing 1  Week'
+          ,Count(DISTINCT BriefingUid) AS Total
+          ,(SELECT Count(DISTINCT BriefingUid)
+             FROM   DailyBriefings
+             WHERE  BriefingAddedBy = '" + id + "' " +
+       "      AND (CAST((SUBSTRING(BriefingDate, 1, CHARINDEX(',', BriefingDate)-1)) as date)) >= dateadd(day, 1-datepart(dw, getdate()), CONVERT(date,getdate())) " +
+       "      AND (CAST((SUBSTRING(BriefingDate, 1, CHARINDEX(',', BriefingDate)-1)) as date)) <  dateadd(day, 8-datepart(dw, getdate()), CONVERT(date,getdate())) " +
+       "      AND (BriefingChecked = 1) " +
+       "      And BriefingDeleted is null or BriefingDeleted = 0 ) AS Done " +
+       "  ,(SELECT Count(DISTINCT BriefingUid) " +
+       "    FROM   DailyBriefings " +
+       "     WHERE  BriefingAddedBy = '" + id + "' " +
+       "      AND (CAST((SUBSTRING(BriefingDate, 1, CHARINDEX(',', BriefingDate)-1)) as date)) >= dateadd(day, 1-datepart(dw, getdate()), CONVERT(date,getdate())) " +
+       "      AND (CAST((SUBSTRING(BriefingDate, 1, CHARINDEX(',', BriefingDate)-1)) as date)) <  dateadd(day, 8-datepart(dw, getdate()), CONVERT(date,getdate())) " +
+       "      AND Isnull(BriefingChecked, '') = '' " +
+       "      and BriefingChecked <> 1 " +
+       "      And BriefingDeleted is null or BriefingDeleted = 0) AS Outstanding " +
+       "FROM   DailyBriefings " +
+       "WHERE  BriefingAddedBy = '" + id + "' " +
+       " AND (CAST((SUBSTRING(BriefingDate, 1, CHARINDEX(',', BriefingDate)-1)) as date)) >= dateadd(day, 1-datepart(dw, getdate()), CONVERT(date,getdate())) " +
+       " AND (CAST((SUBSTRING(BriefingDate, 1, CHARINDEX(',', BriefingDate)-1)) as date)) <  dateadd(day, 8-datepart(dw, getdate()), CONVERT(date,getdate())) " +
+       " And BriefingDeleted is null or BriefingDeleted = 0 " +
+       "UNION " +
+       "SELECT 'Briefing 2 Month' " +
+       "  ,Count(DISTINCT BriefingUid) AS Total  " +
+       "   ,(SELECT Count(DISTINCT BriefingUid) " +
+       "       FROM   DailyBriefings " +
+       "       WHERE  BriefingAddedBy = '" + id + "' " +
+       "        AND Datepart(mm, ( Cast(( Substring(BriefingDate, 1, Charindex(',', BriefingDate) - 1) ) AS DATE) )) = Month(Getdate()) " +
+       "        AND Datepart(yyyy, ( Cast(( Substring(BriefingDate, 1, Charindex(',', BriefingDate ) - 1) ) AS DATE) )) = Year(Getdate()) " +
+       "        AND (BriefingChecked = 1) " +
+       "        And BriefingDeleted is null or BriefingDeleted = 0) AS Done " +
+       "   ,(SELECT Count(DISTINCT BriefingUid) " +
+       "       FROM   DailyBriefings " +
+       "       WHERE  BriefingAddedBy = '" + id + "' " +
+       "        AND Datepart(mm, ( Cast(( Substring(BriefingDate, 1, Charindex(',', BriefingDate) - 1) ) AS DATE) )) = Month(Getdate()) " +
+       "        AND Datepart(yyyy, ( Cast(( Substring(BriefingDate, 1, Charindex(',', BriefingDate ) - 1) ) AS DATE) )) = Year(Getdate()) " +
+       "        AND Isnull(BriefingChecked, '') = '' " +
+       "        AND BriefingChecked <> 1 " +
+       "        And BriefingDeleted is null or BriefingDeleted = 0) AS Outstanding " +
+       "FROM   DailyBriefings " +
+       "WHERE  BriefingAddedBy = '" + id + "' " +
+       "AND Datepart(mm, ( Cast(( Substring(BriefingDate, 1, Charindex(',', BriefingDate) - 1) ) AS DATE) )) = Month(Getdate()) " +
+       "AND Datepart(yyyy, ( Cast(( Substring(BriefingDate, 1, Charindex(',', BriefingDate ) - 1) ) AS DATE) )) = Year(Getdate()) " +
+       " And BriefingDeleted is null or BriefingDeleted = 0 " +
+       "UNION " +
+       "SELECT 'Briefing 3 Older' " +
+       "  ,Count(DISTINCT BriefingUid) AS Total  " +
+       "   ,(SELECT Count(DISTINCT BriefingUid) " +
+       "       FROM   DailyBriefings " +
+       "       WHERE  BriefingAddedBy = '" + id + "' " +
+       "        AND (Datepart(mm, ( Cast(( Substring(BriefingDate, 1, Charindex(',', BriefingDate) - 1) ) AS DATE) )) < Month(Getdate()) " +
+       "             OR Datepart(yyyy, ( Cast(( Substring(BriefingDate, 1, Charindex(',', BriefingDate ) - 1) ) AS DATE) )) < Year(Getdate())) " +
+       "        AND (BriefingChecked = 1) " +
+       "        And BriefingDeleted is null or BriefingDeleted = 0) AS Done " +
+       "   ,(SELECT Count(DISTINCT BriefingUid) " +
+       "       FROM   DailyBriefings " +
+       "       WHERE  BriefingAddedBy = '" + id + "' " +
+       "        AND (Datepart(mm, ( Cast(( Substring(BriefingDate, 1, Charindex(',', BriefingDate) - 1) ) AS DATE) )) < Month(Getdate()) " +
+       "             OR Datepart(yyyy, ( Cast(( Substring(BriefingDate, 1, Charindex(',', BriefingDate ) - 1) ) AS DATE) )) < Year(Getdate())) " +
+       "        AND Isnull(BriefingChecked, '') = '' " +
+       "        AND BriefingChecked <> 1 " +
+       "        And BriefingDeleted is null or BriefingDeleted = 0) AS Outstanding " +
+       "FROM   DailyBriefings " +
+       "WHERE  BriefingAddedBy = '" + id + "' " +
+       "AND (Datepart(mm, ( Cast(( Substring(BriefingDate, 1, Charindex(',', BriefingDate) - 1) ) AS DATE) )) < Month(Getdate()) " +
+       "     OR Datepart(yyyy, ( Cast(( Substring(BriefingDate, 1, Charindex(',', BriefingDate ) - 1) ) AS DATE) )) < Year(Getdate())) " +
+       " And BriefingDeleted is null or BriefingDeleted = 0 ";
+      SqlConnection conn = new SqlConnection(connString);
+      SqlCommand cmd = new SqlCommand(query, conn);
+      SqlDataAdapter da = new SqlDataAdapter(cmd);
+      try
+      {
+        conn.Open();
+        da.Fill(dataTable);
+      }
+      catch (Exception ex)
+      {
+        return BadRequest(ex.Message);
+      }
+
+      conn.Close();
+      da.Dispose();
+
+      if (dataTable == null)
+      {
+        return NotFound();
+      }
+
+      return Ok(dataTable);
+    }
+
+
+  }
+}
---
+++ SignUp/Controllers/Briefings/DailyBriefingsController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using SignUp.Models;
+
+namespace SignUp.Controllers.Briefings
+{
+    public class DailyBriefingsController : ApiController
+    {
+        private Entities db = new Entities();
+
+        // GET: api/DailyBriefings
+        public IQueryable<DailyBriefing> GetDailyBriefings()
+        {
+            return db.DailyBriefings;
+        }
+
+        // GET: api/DailyBriefings/5
+        [ResponseType(typeof(DailyBriefing))]
+        public IHttpActionResult GetDailyBriefing(string id)
+        {
+            DailyBriefing dailyBriefing = db.DailyBriefings.Find(id);
+            if (dailyBriefing == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(dailyBriefing);
+        }
+
+        // PUT: api/DailyBriefings/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutDailyBriefing(string id, DailyBriefing dailyBriefing)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != dailyBriefing.BriefingUid)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(dailyBriefing).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!DailyBriefingExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/DailyBriefings
+        [ResponseType(typeof(DailyBriefing))]
+        public IHttpActionResult PostDailyBriefing(DailyBriefing dailyBriefing)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.DailyBriefings.Add(dailyBriefing);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (DailyBriefingExists(dailyBriefing.BriefingUid))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = dailyBriefing.BriefingUid }, dailyBriefing);
+        }
+
+        // DELETE: api/DailyBriefings/5
+        [ResponseType(typeof(DailyBriefing))]
+        public IHttpActionResult DeleteDailyBriefing(string id)
+        {
+            DailyBriefing dailyBriefing = db.DailyBriefings.Find(id);
+            if (dailyBriefing == null)
+            {
+                return NotFound();
+            }
+
+            db.DailyBriefings.Remove(dailyBriefing);
+            db.SaveChanges();
+
+            return Ok(dailyBriefing);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool DailyBriefingExists(string id)
+        {
+            return db.DailyBriefings.Count(e => e.BriefingUid == id) > 0;
+        }
+    }
+}
